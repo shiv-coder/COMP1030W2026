f5d3df53b8abd26ec30ed823fae22a3f2078f440
shiv-coder <shivi359@gmail.com>
2026-01-12 10:04:49 -0500

    added a line

---
+++ Lab1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World22222222222!");
+        }
+    }
+}
